27b896da1c590f508f382151ce2683958aa3b721
Asjas <asjas@outlook.com>
2017-11-28 14:06:06 +0200

    Changed new game icon to be a gaming controller

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,7 @@ module.exports = {
             2
         ],
         "linebreak-style": [
-            "error",
-            "windows"
+            0
         ],
         "quotes": [
             "error",
