bb980ad4f12e693f204abd41becb39a9a10cbf2d
Asjas <asjas@outlook.com>
2017-12-18 00:42:14 +0200

    configure airbnb eslint and run on all js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,6 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-console": 0,
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            0
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": 0
-    }
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+  },
 };
