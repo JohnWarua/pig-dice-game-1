09d791ffa242b53126996308e75636fa254c92c2
Asjas <asjas@outlook.com>
2018-11-28 15:44:30 +0200

    update build system and remove previous one

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,103 @@
 const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');
+const babelify = require('babelify');
+const browserSync = require('browser-sync').create();
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
 const cleanCSS = require('gulp-clean-css');
-const cssbeautify = require('gulp-cssbeautify');
 const del = require('del');
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const imageminJpegRecompress = require('imagemin-jpeg-recompress');
 const imageminPngquant = require('imagemin-pngquant');
-const livereload = require('gulp-livereload');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const source = require('vinyl-source-stream');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
-// HTML
-gulp.task('reload-html', () => {
-  gulp.src('*.html').pipe(livereload());
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
 });
 
-// Styles
-gulp.task('styles', () =>
-  gulp
-    .src('src/scss/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(plumber(function (err) {
-      console.log('Styles Task Error');
+gulp.task('styles', () => gulp
+  .src('./src/scss/*.scss')
+  .pipe(
+    plumber(function (err) {
       console.log(err);
       this.emit('end');
-    }))
-    .pipe(sass({ style: 'expanded' }))
-    .pipe(cssbeautify({
-      indent: '  ',
-      openbrace: 'end-of-line',
-      autosemicolon: true,
-    }))
-    .pipe(gulp.dest('src/css/'))
-    .pipe(autoprefixer())
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(cleanCSS({ level: '2' }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('../css/', {
+    }),
+  )
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(cleanCSS({ compatibility: 'ie11' }))
+  .pipe(cleanCSS({ level: '2' }))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(
+    sourcemaps.write('./', {
       sourceMappingURL(file) {
         return `${file.relative}.map`;
       },
-    }))
-    .pipe(gulp.dest('public/css/'))
-    .pipe(livereload()));
+    }),
+  )
+  .pipe(gulp.dest('./public/css/')));
 
-// JavaScript
-gulp.task('minify-js', () =>
-  gulp
-    .src('src/js/*.js')
-    .pipe(plumber(function (err) {
-      console.log('JavaScript Task Error');
-      console.log(err);
-      this.emit('end');
-    }))
+gulp.task('minify-js', () => {
+  browserify('./src/js/app.js')
+    .transform(babelify, { presets: ['env'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
     .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['es2015'],
-    }))
+    .pipe(
+      plumber(function (err) {
+        console.log(err);
+        this.emit('end');
+      }),
+    )
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('../js/', {
-      sourceMappingURL(file) {
-        return `${file.relative}.map`;
-      },
-    }))
-    .pipe(gulp.dest('public/js/'))
-    .pipe(livereload()));
-
-// Images
-gulp.task('image-minify', () =>
-  gulp
-    .src('src/images/*')
-    .pipe(imagemin([
-      imagemin.jpegtran(),
-      imagemin.optipng(),
-      imagemin.svgo(),
-      imageminPngquant(),
-      imageminJpegRecompress(),
-    ]))
-    .pipe(gulp.dest('public/images/'))
-    .pipe(livereload()));
-
-// Cleanup public folders
-gulp.task('clean', () => del.sync(['public/css/*', 'public/js/*', 'src/css/*', 'public/images/*']));
-
-// Default task
-gulp.task('default', ['clean', 'reload-html', 'styles', 'minify-js', 'image-minify'], () => {
-  console.log('Running default task');
+    .pipe(
+      sourcemaps.write('./', {
+        sourceMappingURL(file) {
+          return `${file.relative}.map`;
+        },
+      }),
+    )
+    .pipe(gulp.dest('./public/js/'));
 });
 
-// Watch task runner
-gulp.task('watch', ['default'], () => {
+gulp.task('image-minify', () => {
+  gulp
+    .src('src/img/*')
+    .pipe(
+      imagemin([
+        imagemin.jpegtran(),
+        imagemin.optipng(),
+        imagemin.svgo(),
+        imageminPngquant(),
+        imageminJpegRecompress(),
+      ]),
+    )
+    .pipe(gulp.dest('static/'));
+});
+
+// Cleanup public folders
+gulp.task('clean', () => del.sync(['public/css/*', 'public/js/*', 'src/css/*', 'static/*.png', 'static/*.jpg']));
+
+gulp.task('build', ['clean', 'styles', 'minify-js', 'image-minify'], () => {
+  console.log('Building Project.');
+});
+
+gulp.task('watch', ['browser-sync', 'build'], () => {
   console.log('Starting watch task');
-  require('./server.js');
-  livereload.listen();
-  gulp.watch('src/scss/*.scss', ['styles']),
-  gulp.watch('src/js/*.js', ['minify-js']),
-  gulp.watch('*.html', ['reload-html']),
-  gulp.watch('src/images/*', ['image-minify']);
+  gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch('src/scss/*.scss', ['styles']).on('change', browserSync.reload);
+  gulp.watch('src/js/app.js', ['minify-js']).on('change', browserSync.reload);
+  gulp.watch('src/img/*', ['image-minify']);
 });
